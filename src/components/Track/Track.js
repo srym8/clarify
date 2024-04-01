e1182fafbc307c7ef3d399ec67d0408c0dec43b2
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-01 22:23:42 +0100

    Added save to spotify button and empty add/remove functions

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import "./Track.css"
+
 function Track(props) {
+
+    const add = () => {
+
+    }
+
+    const remove = () => {
+
+    }
+
     return (
         <div className="Track">
-            <h3>{props.name}</h3>
+            <h3>{props.name} <button>Save To Spotify</button></h3>
             <p>{props.artist}</p>
             <p>{props.album}</p>
             <p>{props.id}</p>
