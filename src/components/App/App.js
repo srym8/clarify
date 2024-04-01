00882c7dda77103cb4539d6eece2e7e19b31ea67
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-01 22:21:29 +0100

    Mock data readable, and root search function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,23 @@ import SearchResults from "../SearchResults/SearchResults";
 
 
 function App() {
-    const [results, setresults] = useState([{name: "abcdefg", artist: "alpha", album: "letters", id: 1}, {name: "one more time", artist: "jack black", album: "kfp", id: 2}, {name: "one more time", artist: "britany", album: "idk", id: 3}])
+    const [results, setresults] = useState([
+        {name: "abcdef", artist: "alpha", album: "letters", id: 1},
+        {name: "one more time", artist: "jack black", album: "kfp", id: 2},
+        {name: "one more time", artist: "britany", album: "idk", id: 3},
+        {name: "Axel F", artist: "Crazy Frog", album: "idk", id: 4}
+    ])
+
+    const search = () => {
+
+    }
 
     return (
         <div>
             <div className="App">
                 <h1>Clarify</h1>
                 <div className="Search">
-                    <SearchBar />
+                    <SearchBar Search={search}/>
                     <SearchResults results={results}/>
                 </div>
 
