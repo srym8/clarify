f7dbb54cf952def3ce022ca477f9e0f0b10c1287
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-03 21:17:25 +0100

    Added Playlist component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
 
 function App() {
+
     const [results, setresults] = useState([])
+    const [pName, setPName] = useState("New")
+    const [pTracks, setPTracks] = useState([])
 
 
 
@@ -21,15 +25,63 @@ function App() {
         setresults(query)
     }
 
+    const addTrack = (track) => {
+        const alreadyThere = pTracks.some((t) => t.id === track.id);
+
+        if (alreadyThere) {
+            return;
+        } else {
+            setPTracks((prevT) => [...prevT, track]);
+        }
+    }
+
+    const removeTrack = (track) => {
+        setPTracks((prevT) => prevT.filter((t) => t.id !== track.id))
+    }
+
+    const hPName = (n="woah") => {
+
+        setPName(n)
+
+    }
+
+    const test = () => {
+
+
+        const pTracks = [{name: "abcdef", artist: "alpha", album: "letters", id: 1},
+        {name: "one more time", artist: "jack black", album: "kfp", id: 2},
+        {name: "one more time", artist: "britany", album: "idk", id: 3},
+        {name: "Axel F", artist: "Crazy Frog", album: "idk", id: 4},
+        {name: "abcdef", artist: "alpha", album: "letters", id: 1},
+        {name: "one more time", artist: "jack black", album: "kfp", id: 2},
+        {name: "one more time", artist: "britany", album: "idk", id: 3},
+        {name: "Axel F", artist: "Crazy Frog", album: "idk", id: 4}]
+
+        setPTracks(pTracks)
+
+    }
+
     return (
         <div>
             <div className="App">
                 <h1>Clarify</h1>
                 <div className="Search">
+
                     <SearchBar Search={search}/>
-                    <SearchResults results={results}/>
+                    <SearchResults results={results} onAdd={addTrack}/>
+
                 </div>
-                <div className="Playlist"></div>
+                <div className="Playlist">
+
+                    <Playlist
+                    name={pName}
+                    playlistNameChange={hPName}
+                    pTracks={pTracks}
+                    test={test}
+                    onRemove={removeTrack}
+                    />
+
+                </div>
             </div>
         </div>
     )
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import "./Playlist.css";
+
+import TrackList from "../TrackList/TrackList";
+
 function Playlist(props) {
+
+    const handle = (e) => {
+        props.playlistNameChange(e.target.value);
+    }
+
     return (
         <div className="Playlist">
-
+            <input defaultValue={"New"} onChange={handle}></input>
+            <TrackList
+            tracks={props.pTracks}
+            removeT={props.onRemove}
+            isPlaylist={true}
+            />
+            <button className="PlaylistSave" onClick={props.test}>Save To Spotify</button>
         </div>
     )
 }
