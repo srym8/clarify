207486ffa26f569f281cf48fe99a25b2c654a18f
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-07 19:21:22 +0100

    Added cleanup

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import App from "../App/App";
 
+afterEach(() => {
+    cleanup();
+})
 
 test("should render app component", () => {
     render(<App />)
