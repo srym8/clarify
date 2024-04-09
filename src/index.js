8420345b7b7ac7327ca41562fea1f456598650a3
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-09 17:49:13 +0100

    Fixing damage by trying to install react router DOM, will use another branch

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import Login from './components/Login/Login';
 import reportWebVitals from './reportWebVitals';
-
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
-
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={ <Login /> }>
-    <Route path="/app" element={ <App /> } />
-  </Route>
-))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
