f70a2a3712cd1d5ac758ba1fb80429502569eb5c
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-09 18:23:15 +0100

    Adding 404 page

---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NotFoundPage() {
+    return (
+        <><main>404 Not Found</main><Link to="/">Back To Login</Link></>
+    )
+}
+
+export default NotFoundPage
