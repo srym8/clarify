dfffc5272fe15e6b9e088a1bb1a43057328ba5cc
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-09 17:49:43 +0100

    Adding skeleton tests

--- src/components/__tests__/Playlist.test.js
+++ src/components/__tests__/Playlist.test.js
@@ -1,5 +1,14 @@
 import { render, screen, cleanup } from "@testing-library/react";
+import Playlist from "../Playlist/Playlist";
+import TrackList from "../TrackList/TrackList";
+import Track from "../Track/Track";
 
 afterEach(() => {
     cleanup();
+})
+
+test("renders playlist component", () => {
+    render(<Playlist />)
+    const PlaylistElement = screen.getByTestId("playlist-1")
+    expect(PlaylistElement).toBeInTheDocument();
 })
--- src/components/__tests__/Track.test.js
+++ src/components/__tests__/Track.test.js
@@ -1,5 +1,14 @@
 import { render, screen, cleanup } from "@testing-library/react";
 
+import Track from "../Track/Track";
+
 afterEach(() => {
     cleanup();
+})
+
+test("renders track component", () => {
+    const track = { name: "wow", artists: "no way", album: "woah" }
+    render(<Track name={track.name} artists={track.artists} album={track.album}/>)
+    const TrackElement = screen.getByTestId("track-1")
+    expect(TrackElement).toHaveTextContent("wow");
 })
