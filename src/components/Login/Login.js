40bb8acfdefc83e1334a7b40fe4ec0eaa8e5131e
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-04-12 21:06:45 +0100

    Added login handling. Basic handling (if email and password are equal then it redirects to app page)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import "./Login.css";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+
+    const handleEmail = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (p) => {
+        p.preventDefault();
+        setPassword(p.target.value)
+    }
+
+    let navigate = useNavigate();
+
+    const handleSubmit = () => {
+        if (email === password) {
+            navigate("/app")
+        }
+    }
+
     return (
         <div className='Login'>
-            <form id='login-form'>
-                <input className='field' placeholder='Enter Email'></input>
-                <input className='field' placeholder='Enter Password'></input>
+            <form id='login-form' onSubmit={handleSubmit}>
+                <input className='field' placeholder='Enter Email' value={email} onChange={handleEmail}></input>
+                <input className='field' placeholder='Enter Password' value={password} onChange={handlePassword}></input>
                 <button type='submit' form='login-form'>Login</button>
                 <Link to="/app">To app</Link>
             </form>
