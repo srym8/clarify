6a3443883b41c3dc9cc455200ada0e22faa3b7bc
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-07-02 18:33:25 +0100

    Importing Spotify.js to Login.js to implement logging in via Spotify

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import Spotify from '../../Spotify/Spotify';
+
 import "./Login.css";
 
 function Login() {
