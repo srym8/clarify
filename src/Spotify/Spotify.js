eaa1c655b4098cac83ab7d806edc7b6faf4a5c03
Adam Muqtadir <adam.muqtadir@outlook.com>
2024-10-02 22:37:58 +0100

    Just had to add something to previous commit

--- src/Spotify/Spotify.js
+++ src/Spotify/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = "4718e4f536b64fcda2004650287911a0";
-const redirectUri = 'http://localhost:3000/app';
+const redirectUri = 'https://clarify42.netlify.app/';
 let accessToken = '';
 
 const Spotify = {
